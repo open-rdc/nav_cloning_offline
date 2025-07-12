e1d865e3b794422261ab7008e099d53487c459dc
KOYAMA-Yuya <vulpes.yuya@gmail.com>
2025-07-13 01:17:43 +0900

    run_collect_3cam_real 試し中

--- scripts/data_collect/run_collect_3cam_real.py
+++ scripts/data_collect/run_collect_3cam_real.py
@@ -6,22 +6,15 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from skimage.transform import resize
 from geometry_msgs.msg import Twist
-from geometry_msgs.msg import PoseArray
-from std_msgs.msg import Int8
-from std_srvs.srv import Trigger
-from nav_msgs.msg import Path
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from std_srvs.srv import Empty
+from nav_msgs.msg import Path, Odometry
 from std_srvs.srv import SetBool, SetBoolResponse
 import csv
 import os
 import time
-import copy
-from nav_msgs.msg import Odometry
-import math
+import numpy as np
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../pytorch'))
 from nav_cloning_pytorch import *
 
@@ -32,69 +25,49 @@ class cource_following_learning_node:
         print("action_num: " + str(self.action_num))
         self.bridge = CvBridge()
 
-        #カメラからのデータをサブスクライブ
-        self.image_sub = rospy.Subscriber("/camera/lane1/center/rgb/image_raw", Image, self.callback)
-        self.image_sub = rospy.Subscriber("/camera/lane2/center/rgb/image_raw", Image, self.callback_left_camera)
-        self.image_sub = rospy.Subscriber("/camera/lane3/center/rgb/image_raw", Image, self.callback_right_camera)
-        
+        # カメラデータ購読
+        self.image_sub_center = rospy.Subscriber("/camera_center/image_raw", Image, self.callback)
+        self.image_sub_left   = rospy.Subscriber("/camera_left/image_raw", Image, self.callback_left_camera)
+        self.image_sub_right  = rospy.Subscriber("/camera_right/image_raw", Image, self.callback_right_camera)
+
         self.vel_sub = rospy.Subscriber("/icart_mini/cmd_vel", Twist, self.callback_vel, queue_size=10)
-        self.odom_sub = rospy.Subscriber("/odom", Odometry, self.callback_odom)
-        self.path_sub = rospy.Subscriber("/move_base/NavfnROS/plan", Path, self.callback_path)
-        self.min_distance = 0.0
+        
+        self.cv_image = np.zeros((520, 694, 3), np.uint8)
+        self.cv_left_image = np.zeros((520, 694, 3), np.uint8)
+        self.cv_right_image = np.zeros((520, 694, 3), np.uint8)
+
+        self.vel = Twist()
         self.action = 0.0
         self.episode = 0
-        self.vel = Twist()
-        self.path_pose = PoseArray()
-        self.cv_image = np.zeros((520,694,3), np.uint8)
-        self.cv_left_image = np.zeros((520,694,3), np.uint8)
-        self.cv_right_image = np.zeros((520,694,3), np.uint8)
-        self.learning = True
-        self.select_dl = False
-        self.start_time = time.strftime("%Y%m%d_%H:%M:%S")
-        self.path = roslib.packages.get_pkg_dir('nav_cloning') + '/data/result/'
-        self.save_path = roslib.packages.get_pkg_dir('nav_cloning') + '/data/model/'
-        self.previous_reset_time = 0
-        self.start_time_s = rospy.get_time()
         self.save_img_no = 0
-        self.save_img_left_no = 1
-        self.save_img_center_no = 0
-        self.save_img_right_no = -1
+
+        self.start_time = time.strftime("%Y%m%d_%H:%M:%S")
         self.path = roslib.packages.get_pkg_dir('nav_cloning') + '/data/'
-        self.pose_x = 0
-        self.pose_y = 0
-        self.old_pose_x = 0
-        self.old_pose_y = 0
-        self.current_pose_x = 0
-        self.current_pose_y = 0
-        self.flag = False
+
         os.makedirs(self.path + "img/" + self.start_time)
         os.makedirs(self.path + "ang/" + self.start_time)
-        self.path_pose_x = 0
-        self.path_pose_y = 0
-        self.path_no = 0
 
-    #キャプチャと保存
     def capture_img(self):
-            Flag = True
-            try:
-                #カメラ画像を保存
-                cv2.imwrite(self.path + "img/" + self.start_time + "/left" + str(self.save_img_no) + ".jpg", self.resize_left_img)
-                cv2.imwrite(self.path + "img/" + self.start_time + "/center" + str(self.save_img_no) + ".jpg", self.resize_img)
-                cv2.imwrite(self.path + "img/" + self.start_time + "/right" + str(self.save_img_no) + ".jpg", self.resize_right_img)
-                
-            except:
-                print('Not save image')
-                Flag = False
-            finally:
-                if Flag:
-                    print('Save image Number:', self.save_img_no)
+        try:
+            # 画像リサイズ
+            self.resize_left_img = cv2.resize(self.cv_left_image, (64, 48), interpolation=cv2.INTER_AREA)
+            self.resize_img = cv2.resize(self.cv_image, (64, 48), interpolation=cv2.INTER_AREA)
+            self.resize_right_img = cv2.resize(self.cv_right_image, (64, 48), interpolation=cv2.INTER_AREA)
 
-    #角速度保存
+            # 保存
+            cv2.imwrite(self.path + "img/" + self.start_time + "/left" + str(self.save_img_no) + ".jpg", self.resize_left_img)
+            cv2.imwrite(self.path + "img/" + self.start_time + "/center" + str(self.save_img_no) + ".jpg", self.resize_img)
+            cv2.imwrite(self.path + "img/" + self.start_time + "/right" + str(self.save_img_no) + ".jpg", self.resize_right_img)
+
+            print('Saved image number:', self.save_img_no)
+        except:
+            print('Failed to save image')
+
     def capture_ang(self):
-            line = [str(self.save_img_no), str(self.action)]
-            with open(self.path + "ang/" + self.start_time + '/ang.csv', 'a') as f:
-                writer = csv.writer(f, lineterminator='\n')
-                writer.writerow(line)
+        line = [str(self.save_img_no), str(self.action)]
+        with open(self.path + "ang/" + self.start_time + '/ang.csv', 'a') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(line)
 
     def callback(self, data):
         try:
@@ -114,47 +87,12 @@ class cource_following_learning_node:
         except CvBridgeError as e:
             print(e)
 
-    def callback_path(self, data):
-        self.path_pose = data
-
-    def check_distance(self):
-        distance = math.sqrt((self.pose_x - self.old_pose_x)**2 + (self.pose_y - self.old_pose_y)**2)
-        print("distance : ", distance)
-        if distance >= 0.05:
-            self.old_pose_x = self.pose_x
-            self.old_pose_y = self.pose_y
-            self.flag = True
-
-    def callback_odom(self, data):
-        self.pose_x = data.pose.pose.position.x
-        self.pose_y = data.pose.pose.position.y      
-
-    #移動の角速度を格納
     def callback_vel(self, data):
         self.vel = data
         self.action = self.vel.angular.z
 
-    def callback_dl_training(self, data):
-        resp = SetBoolResponse()
-        self.learning = data.data
-        resp.message = "Training: " + str(self.learning)
-        resp.success = True
-        return resp
-
     def loop(self):
-        self.check_distance()
-        if self.flag:
-
-            # 画像リサイズ            
-            self.resize_left_img = cv2.resize(self.cv_left_image, (64, 48), interpolation=cv2.INTER_AREA)
-            self.resize_img = cv2.resize(self.cv_image, (64, 48), interpolation=cv2.INTER_AREA)
-            self.resize_right_img = cv2.resize(self.cv_right_image, (64, 48), interpolation=cv2.INTER_AREA)
-    
-            self.capture_img()
-            self.capture_ang()
-            self.save_img_no += 1
-            self.flag = False
-        
+        # 画像がまだ届いていない場合はスキップ
         if self.cv_image.size != 694 * 520 * 3:
             return
         if self.cv_left_image.size != 694 * 520 * 3:
@@ -162,12 +100,18 @@ class cource_following_learning_node:
         if self.cv_right_image.size != 694 * 520 * 3:
             return
 
+        # 保存
+        self.capture_img()
+        self.capture_ang()
+        self.save_img_no += 1
+
+        # 学習終了条件（任意）
         if self.episode == 4000:
             self.learning = False
 
 if __name__ == '__main__':
     rg = cource_following_learning_node()
-    DURATION = 0.1
+    DURATION = 0.5  # 保存間隔（秒）
     r = rospy.Rate(1 / DURATION)
     while not rospy.is_shutdown():
         rg.loop()
